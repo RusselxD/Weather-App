cf5040a2cd113183de848ac0b0e5396f7e56142c
RusselxD <russelcabigquez8@gmail.com>
2025-01-01 23:51:12 +0800

    i think that's it

--- src/components/nav-bar/index.jsx
+++ src/components/nav-bar/index.jsx
@@ -2,10 +2,10 @@ import homeIcon from "../../assets/home.png";
 
 export default function NavBar() {
     return (
-        <nav className="bg-sec-color w-20 h-full rounded-2xl overflow-hidden">
+        <nav className="bg-sec-color w-16 h-full rounded-2xl overflow-hidden">
             <img
                 style={{ backgroundColor: "rgba(255, 255, 255, 0.1)" }}
-                className="mb-6 p-6 w-full aspect-square"
+                className="mb-6 p-5 w-full aspect-square"
                 src={homeIcon}
             ></img>
         </nav>
